953bbfd769c515e589d4ce9d490a29ebda9c52f2
Theo Vora <theo.vora@gmail.com>
2020-10-27 18:28:33 -0600

    Fade in image of dog

--- AnimateFadeIn/ViewController.swift
+++ AnimateFadeIn/ViewController.swift
@@ -8,12 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    // MARK: - IBOutlets
+    @IBOutlet weak var dogImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        dogImageView.alpha = 0.0
+        
+        //        UIView.animate(withDuration: 6.0) {
+        //            self.dogImageView.alpha = 1.0
+        //        }
+        
+        UIView.animate(withDuration: 3.0,
+                       delay: 3.0,
+                       options: .curveEaseIn,
+                       animations: {
+                        self.dogImageView.alpha = 1.0
+                       },
+                       completion: nil)
+        
+        
     }
-
-
+    
+    
 }
 
